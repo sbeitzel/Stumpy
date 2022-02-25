07122f57288579bc5f7065acd12bc8bf13673e70
Stephen Beitzel <sbeitzel@pobox.com>
2022-02-25 09:24:19 -0800

    Allow changing the size of a mail store

--- Stumpy/Views/MailstoreControlView.swift
+++ Stumpy/Views/MailstoreControlView.swift
@@ -19,6 +19,7 @@ struct MailstoreControlView: View {
     @State private var smtpPortString: String
     @State private var popPortString: String
     @State private var messageCountString: String
+    @State private var mailSlots: String
     @State private var smtpConnected = 0
     @State private var popConnected = 0
     @State private var allowMultipleMail = true
@@ -36,51 +37,75 @@ struct MailstoreControlView: View {
         _smtpPortString = State(wrappedValue: String(smtpServer.serverPort))
         _popPortString = State(wrappedValue: String(popServer.serverPort))
         _messageCountString = State(wrappedValue: "0")
+        _mailSlots = State(wrappedValue: String(serverSpec.mailSlots))
         self.serverSpec = serverSpec
     }
 
     var body: some View {
         HStack {
             VStack {
+                Button(action: buttonAction) { buttonLabel }
+                .frame(width: 55, height: 30)
+                Circle()
+                    .fill(Color(smtpServer.isRunning ? .green : .red))
+                    .frame(width: 55,
+                           height: 30)
+                Spacer()
+            }
+            VStack {
                 HStack(alignment: .top) {
-                    Button(action: buttonAction) { buttonLabel }
-                    .frame(width: 55, height: 30)
                     VStack {
-                        Toggle(isOn: $allowMultipleMail.maybeChange({ newValue in
-                            return smtpServer.allowMultipleEmails(newValue)
-                        }).onChange {
-                            setAllowMultipleMail()
-                        }) {
-                            Text("Allow multiple MAIL transactions per session")
-                        }.disabled(smtpServer.isRunning)
                         HStack {
-                            Text("SMTP Port:")
-                            TextField("SMTP Port",
-                                      text: $smtpPortString.onChange(setSMTPPort).validate(validateSMTPPort))
+                            Spacer()
+                            Toggle(isOn: $allowMultipleMail.maybeChange({ newValue in
+                                return smtpServer.allowMultipleEmails(newValue)
+                            }).onChange {
+                                setAllowMultipleMail()
+                            }) {
+                                Text("Allow multiple MAIL transactions per session")
+                            }.disabled(smtpServer.isRunning)
+                        }
+                        HStack {
+                            Spacer()
+                            Text("Mail store size:")
+                            TextField("mail slots", text: $mailSlots.onChange(setMailSlots).validate(validateMailSlots))
+                                .disabled(smtpServer.isRunning)
                                 .frame(minWidth: 60,
                                        idealWidth: 80,
                                        maxWidth: 80,
                                        minHeight: 20,
                                        idealHeight: 25,
                                        maxHeight: 30,
-                                       alignment: .center)
+                                       alignment: .trailing)
+                        }
+                        HStack {
+                            Spacer()
+                            Text("SMTP Port:")
+                            TextField("SMTP Port",
+                                      text: $smtpPortString.onChange(setSMTPPort).validate(validateSMTPPort))
+                                .disabled(smtpServer.isRunning)
+                                .frame(minWidth: 60,
+                                       idealWidth: 80,
+                                       maxWidth: 80,
+                                       minHeight: 20,
+                                       idealHeight: 25,
+                                       maxHeight: 30,
+                                       alignment: .trailing)
                         }
                     }
                 }
                 HStack {
-                    Circle()
-                        .fill(Color(smtpServer.isRunning ? .green : .red))
-                        .frame(width: 55,
-                               height: 30)
+                    Spacer()
                     Text("POP3 Port:")
                     TextField("POP3 Port", text: $popPortString.onChange(setPOPPort).validate(validatePOPPort))
+                        .disabled(popServer.isRunning)
                         .frame(minWidth: 60,
                                idealWidth: 80,
                                maxWidth: 80,
                                minHeight: 20,
                                idealHeight: 25,
                                maxHeight: 30,
-                               alignment: .center)
+                               alignment: .trailing)
                 }
             }
             .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 0))
@@ -116,6 +141,7 @@ struct MailstoreControlView: View {
                                         leading: 0,
                                         bottom: 5,
                                         trailing: 5))
+                Spacer()
             }
             .padding()
             Spacer()
@@ -126,6 +152,26 @@ struct MailstoreControlView: View {
 
     private func setAllowMultipleMail() {
         serverSpec.allowMultiMail = allowMultipleMail
+    }
+
+    private func setMailSlots() {
+        guard let slots = Int32(mailSlots) else {
+            return
+        }
+        Task {
+            DispatchQueue.main.async {
+                serverSpec.mailSlots = slots
+            }
+            await mailStore.adjustSize(to: Int(slots))
+        }
+    }
+
+    private func validateMailSlots(_ value: String) -> Bool {
+        guard let slots = Int32(value) else {
+            return false
+        }
+
+        return slots > 0 && !smtpServer.isRunning && !popServer.isRunning
     }
 
     private func setSMTPPort() {
@@ -149,7 +195,7 @@ struct MailstoreControlView: View {
             // not a number
             return false
         }
-        guard port <= Int16.max else {
+        guard port <= Int16.max && port > 0 else {
             // number is too big
             return false
         }
@@ -162,7 +208,7 @@ struct MailstoreControlView: View {
             // not a number
             return false
         }
-        guard port <= Int16.max else {
+        guard port <= Int16.max && port > 0 else {
             // number is too big
             return false
         }
